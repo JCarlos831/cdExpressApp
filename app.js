9a63bc911c8a7224bffecdc1f414271f28caa410
Juan Montoya <jcmontoya@gmail.com>
2018-01-24 00:53:37 +0000

    Needed to go back to level 1

--- app.js
+++ app.js
@@ -3,31 +3,35 @@ var app = express();
 var portNum = process.env.PORT;
 var currentDate = new Date();
 
-// app.get('/', function(request, response) {
-//     response.send("Hello World");
-// });
+app.get('/', function(request, response) {
+    response.send("Hello World");
+});
 
-// app.get('/name', function(request, response) {
-//     response.send("Juan Montoya");
-// });
+app.get('/name', function(request, response) {
+    response.send("Juan Montoya");
+});
 
-// app.get('/redirect', function(request, response){
-//   response.redirect(301, '/surprise');
-// });
+app.get('/redirect', function(request, response){
+  response.redirect(301, '/surprise');
+});
 
-// app.get('/', function(request, response){
-//     response.send(currentDate);
-// });
+app.get('/date', function(request, response){
+    response.send(currentDate);
+});
 
 app.use(express.static('public'));
 
 app.get('/cities', function(request, response){
-  var cities = ['Providence', 'Warwick', 'Pawtucket', 'Central Falls'];
+  var cities = ['Providence', 'Warwick', 'Pawtucket', 'Central Falls', 'Cumberland'];
   response.json(cities);
 });
 
-// app.get('/index', function(request, response){
-//     response.send();
-// });
+app.get('/index', function(request, response){
+    response.send();
+});
+
+app.get('/surprise', function(request, response){
+   response.send("SURPRISE!!!!");
+});
 
 app.listen(portNum);
