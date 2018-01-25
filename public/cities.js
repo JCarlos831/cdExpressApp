6b428de254e6a62dcf28bcebdde381c5397b84b6
Juan Montoya <jcmontoya@gmail.com>
2018-01-25 20:34:35 +0000

    Created drop down menu for cities

--- public/cities.js
+++ public/cities.js
@@ -1,12 +1,11 @@
 /* global $ cities*/
-
 $(function(){
     $.get('/cities', appendToList);
     
     function appendToList(cities){
         var list = [];
         for (var i in cities){
-            list.push($('<input>', {placeholder: cities[i]}));
+            list.push($('<option>', {text: cities[i]}));
         }
         console.log(list);
         $('.cityList').append(list);
