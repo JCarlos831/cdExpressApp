9b3ca0d6aad0cca85360560c92467d84788eadfe
Juan Montoya <jcmontoya@gmail.com>
2018-01-29 00:03:36 +0000

    404 Status Error now working

--- app.js
+++ app.js
@@ -4,17 +4,15 @@ var portNum = process.env.PORT;
 
 app.use(express.static('public'));
 
-var cities = {
-      'Providence': 'Rhode Island',
-      'Boston': 'Massachusetts',
-      'Portland': 'Maine',
-      'San Diego': 'California',
-      'Houston' : 'Texas'
-  };
-  
-app.get('/cities/:name', function(request, response){
-    var description = cities[request.params.name];
-    response.json(description);
+app.get('/cities', function(request, response) {
+  var cities = ['Providence', 'Boston', 'Portland', 'San Diego', 'Austin'];
+  if(request.query.limit > cities.length){
+    response.status(404).json("Error");
+  } else if(request.query.limit > 0) {
+    response.json(cities.slice(0, request.query.limit));
+  } else {
+    response.json(cities);
+  }
 });
 
 app.listen(portNum);
