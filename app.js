84fa9e8b19288e4261c00b896afae207adfc26aa
Juan Montoya <jcmontoya@gmail.com>
2018-02-01 01:00:04 +0000

    Continued work on level 4

--- app.js
+++ app.js
@@ -1,76 +1,50 @@
 var express = require('express');
 var app = express();
-var portNum = process.env.PORT;
-var currentDate = new Date();
+
 var bodyParser = require('body-parser');
-var parseUrlencoded = bodyParser.urlencoded({ extended: false });
+var parseUrlencoded = bodyParser.urlencoded({extended: false});
+
+var blocks = {
+    'Fixed': 'Fastened securely in position',
+    'Movable': 'Capable of being moved',
+    'Rotating': 'Moving in a circle around its center'
+};
+
+app.post('/blocks', parseUrlencoded, function(request, response) {
+    var newBlock = request.body;
+    blocks[newBlock.name] = newBlock.description;
+    response.status(201).json(newBlock.name);
+});
 
 app.use(express.static('public'));
 
-var cities = {
-    'Providence': 'Rhode Island', 
-    'Boston': 'Massachusetts',
-    'Portland': 'Maine',
-    'Austin': 'Texas',
-    'Oakland': 'California'
-  };
-  
-app.delete('/cities/:city', function(request, response){
-  delete cities[request.cityNameFixed];
-  response.sendStatus(200);
+app.param('name', function(request, response, next) {
+    var name = request.params.name;
+    var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
+    
+    request.blockName = block;
+    
+    next();
 });
 
-app.post('/cities', parseUrlencoded, function(request, response){
-  if(request.body.city.length > 4 && request.body.state.length > 2){
-  var newCity = request.body;
-  cities[newCity.city] = newCity.state;
-  response.status(201).json(newCity.name);
-  }
-});
-  
-app.param('city', function(request, response, next){
-  var city = request.params.city;
-  var cityName = city[0].toUpperCase() + city.slice(1).toLowerCase();
-  
-  request.cityNameFixed = cityName;
-  
-  next();
+app.get('/blocks', function(request, response) {
+    response.json(Object.keys(blocks));
 });
 
-app.get('/cities', function(request, response){
-    if(request.query.limit > cities.length){
-    response.status(404).json("Error");
-  } else if(request.query.limit > 0) {
-    response.json(Object.keys(cities).slice(0, request.query.limit));
-  } else {
-    response.json(Object.keys(cities));
-  }
+app.get('/blocks/:name', function(request, response){
+    var description = blocks[request.blockName];
+    
+    if (!description) {
+        response.status(404).json('No description found for ' + request.params.name);
+    } else {
+        response.json(description);
+    }
+    
+    if (request.query.limit >= 0) {
+        response.json(blocks.slice(0, request.query.limit));
+    } else {
+        response.json(blocks);
+    }
 });
 
-app.get('/cities/:city', function(request, response) {
-  var state = cities[request.cityNameFixed];
-  
-  if(!state){
-    response.status(404).json('There are no results for ' + request.params.city);
-  }else{
-    response.json(state);
-  }
-});
-
-app.get('/name', function(request, response) {
-    response.send("Juan Montoya");
-});
-
-app.get('/redirect', function(request, response){
-  response.redirect(301, '/surprise');
-});
-
-app.get('/surprise', function(request, response){
-    response.send("SURPRISE!!!");
-});
-
-app.get('/date', function(request, response){
-    response.send(currentDate);
-});
-
-app.listen(portNum);
+app.listen(process.env.PORT);
